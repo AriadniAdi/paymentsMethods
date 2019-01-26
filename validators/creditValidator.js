92d09f990519f11e55fe8183f44a83494325e39d
AriadniAdi <ariadniadi@gmail.com>
2019-01-26 04:02:49 -0200

    adding more credit card validations

--- validators/creditValidator.js
+++ validators/creditValidator.js
@@ -4,7 +4,7 @@ class CreditValidate {
     static validateCreditCard(data) {
 
         if(!data.card.cardType) {
-            return 'Missing card number';
+            return 'Missing card type';
         }
         if(!data.card.number) {
             return 'Missing card number';
@@ -15,25 +15,27 @@ class CreditValidate {
         if(!data.card.expiryYear) {
             return 'Missing expiry year';
         }
-        
+
+        data.card["cardType"] = data.card["cardType"].toUpperCase();
+
+        if(!['MASTERCARD', 'VISA', 'AMERICANEXPRESS', 'JBC', 'DINERSCLUB'].includes(data.card.cardType)) {
+            return 'invalid card type'
+        }
+
         var validation = CreditCard.validate(data.card)
 
         if(!validation.validCvv) {
             return 'invalid ccv';
         }
-        
         if(!validation.validCardNumber) {
-            return 'invalid card number';
+            return 'invalid card number'
         }
-
         if(!validation.validExpiryMonth) {
-            return 'invalid expiry month';
+            return 'invalid expiry month'
         }
-
         if(!validation.validExpiryYear) {
-            return 'invalid expiry year';
+            return 'invalid expiry year'
         }
-
     }
 }
 
